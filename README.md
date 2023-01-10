3c2fd473a24b3ea53bccc1fa2dca859353f59b06
JuniorCostaDev <joel_br_junior@hotmail.com>
2023-01-10 18:52:46 -0300

    init: inicio do projeto

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nodets-canil
+Projeto feito no módulo 2 do curso Node + Typescript B7web
+
+### Pré-requisitos globais:
+`npm i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### Para rodar o projeto
+`npm run start-dev`
---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mustacheExpress from 'mustache-express';
+import path from 'path';
+
+dotenv.config();
+
+const server = express();
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustacheExpress());
+
+server.use(express.static(path.join(__dirname, '../public')));
+
+//Rotas
+
+server.listen(process.env.PORT);
